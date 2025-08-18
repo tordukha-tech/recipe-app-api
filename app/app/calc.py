6cb4f594a95252e00bfa8a0a257e96cc1787bce6
Teymur R Ordukhanov <YOUR_ID+USERNAME@users.noreply.github.com>
2025-08-18 22:33:54 +0400

    Scaffold db dependencies

---
+++ app/app/calc.py
@@ -0,0 +1,25 @@
+"""
+    Calculator functions
+"""
+
+
+def add(x, y):
+    """Add two numbers."""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract two numbers."""
+    return x - y
+
+
+# def multiply(x, y):
+#     """Multiply two numbers."""
+#     return x * y
+
+
+# def divide(x, y):
+#     """Divide two numbers."""
+#     if y == 0:
+#         return None
+#     return x / y
