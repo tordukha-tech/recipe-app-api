59101fedffb500e4a8aa489efcf5f8d448807d5b
Teymur R Ordukhanov <YOUR_ID+USERNAME@users.noreply.github.com>
2025-08-21 01:25:17 +0400

    first migration and model

---
+++ app/core/test_models.py
@@ -0,0 +1,25 @@
+"""
+    Tests for models.
+"""
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+    """Test Models."""
+
+    def setUp(self):
+        self.User = get_user_model()
+
+    def test_create_user_with_with_email_successful(self):
+        """Test creating a user with an email is successfull."""
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
